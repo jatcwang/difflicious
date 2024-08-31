8e3a27b85e3ecc213af9bfe38fc5a5f7014d89b8
Jacob Wang <jatcwang@gmail.com>
2024-08-31 09:24:20 +0100

    Upgrade to munit 1.0.1

--- modules/coretest/src/test/scala-3/difflicious/DifferAutoDerivationSpec.scala
+++ modules/coretest/src/test/scala-3/difflicious/DifferAutoDerivationSpec.scala
@@ -1,10 +1,12 @@
 package difflicioustest
 
 import munit.ScalaCheckSuite
+import DifferAutoDerivationSpec.P1
 
 class DifferAutoDerivationSpec extends ScalaCheckSuite {
   test("should not compile without instance in scope") {
-    val result = compileErrors("""
+    val result = compileErrors(
+      """
         import difflicious._
         final case class P1(f1: String)
         
@@ -14,35 +16,28 @@ class DifferAutoDerivationSpec extends ScalaCheckSuite {
         """)
     assertNoDiff(
       result,
-      """error:
-        |No given instance of type difflicious.Differ[P1] was found for parameter differ of method apply in object Differ
-        |
-        |The following import might fix the problem:
-        |
-        |  import difflicious.generic.auto.autoDerived
-        |
+      """error: No given instance of type difflicious.Differ[P1] was found for parameter differ of method apply in object Differ
         |        Differ[P1].diff(P1("a"), P1("b"))
         |                 ^
         |""".stripMargin
     )
   }
   test("should find auto derived instance for product") {
-    val result = compileErrors("""
+    val result = compileErrors(
+      """
         import difflicious._
         import difflicious.generic.auto.given
-
-        final case class P1(f1: String)
         
         Differ[P1].diff(P1("a"), P1("b"))
         """)
     assertNoDiff(result, "")
   }
   test("should put auto derived instance back into scope") {
-    val result = compileErrors("""
+    val result = compileErrors(
+      """
         import difflicious._
         import difflicious.generic.auto.given
 
-        final case class P1(f1: String)
         implicit val d: Differ[P1] = implicitly[Derived[P1]].differ
         
         Differ[P1].diff(P1("a"), P1("b"))
@@ -54,11 +49,15 @@ class DifferAutoDerivationSpec extends ScalaCheckSuite {
     import difflicious.generic.auto._
     import difflicious.generic.auto.given
 
-    final case class P1(f1: String, f2: String)
-    final case class P2(p1: P1)
-    implicit val d: Differ[P1] = implicitly[Derived[P1]].differ.ignoreAt(_.f1)
+    final case class Pi(f1: String, f2: String)
+    final case class P2(p1: Pi)
+    implicit val d: Differ[Pi] = implicitly[Derived[Pi]].differ.ignoreAt(_.f1)
 
-    val r = Differ[P2].diff(P2(P1("a", "a")), P2(P1("b", "a")))
+    val r = Differ[P2].diff(P2(Pi("a", "a")), P2(Pi("b", "a")))
     assertEquals(r.isOk, true)
   }
+}
+  
+object DifferAutoDerivationSpec {
+  final case class P1(f1: String)
 }
