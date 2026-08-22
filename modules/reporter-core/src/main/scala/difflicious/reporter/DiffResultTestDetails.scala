a8875402cc5d737196dd4fe3a94b13374424a76e
Jacob Wang <jatcwang@gmail.com>
2026-08-22 16:58:20 +0100

    Fix flaky reporter codec compilation

--- modules/reporter-core/src/main/scala/difflicious/reporter/DiffResultTestDetails.scala
+++ modules/reporter-core/src/main/scala/difflicious/reporter/DiffResultTestDetails.scala
@@ -1,10 +1,7 @@
 package difflicious.reporter
 
 import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, readFromString}
-import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodecMaker}
 import difflicious.DiffResult
-
-import scala.annotation.nowarn
 
 /** Information regarding a diff test failure, such as the name of the test suite and test. The data in this case class
   * are serialized by the reporter
@@ -28,15 +25,6 @@ object DiffResultTestDetails {
   def fromJsonString(json: String): DiffResultTestDetails =
     readFromString(json)
 
-  @nowarn("msg=match may not be exhaustive")
-  implicit lazy val jsonValueCodec: JsonValueCodec[DiffResultTestDetails] = {
-    implicit val diffResultJsonValueCodec: JsonValueCodec[DiffResult] =
-      DiffResultJson.diffResultJsonValueCodec
-
-    JsonCodecMaker.make[DiffResultTestDetails](
-      CodecMakerConfig
-        .withTransientNone(false)
-        .withTransientDefault(false),
-    )
-  }
+  implicit lazy val jsonValueCodec: JsonValueCodec[DiffResultTestDetails] =
+    DiffResultTestDetailsJson.jsonValueCodec
 }
